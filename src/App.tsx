b2623460f67b634339fecaeeabc98a2bf268a576
Antoine Cusset <a.cusset@gmail.com>
2022-03-17 21:21:45 +0800

    Shrek collab

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
 import zero from './zero.png';
+import shrek from './shrek.png';
 import './App.css';
 
 function App() {
-  const audio = new Audio('sub-and-non-sub.wav')
+  const subAudio = new Audio('sub-and-non-sub.wav')
+  const shrekAudio = new Audio('shrek-collab.mp3')
 
-  const handleClick = () => {
-    console.log('clic')
-    audio.play()
+  const handleClick = (title: string) => {
+    switch (title) {
+      case 'shrek':
+        return shrekAudio.play()
+      case 'sub':
+        return subAudio.play()
+    }
   }
 
   return (
     <div className="App">
-      <header onClick={handleClick} className="App-header">
-        <img src={zero} alt="logo" className='App-logo' />
+      <header className="App-header">
+        <div  onClick={() => handleClick('sub')} >
+          <img src={zero} alt="logo" className='App-logo' />
+        </div>
+        <div onClick={() => handleClick('shrek')} >
+          <img src={shrek} alt="logo" className='App-logo' />
+        </div>
       </header>
     </div>
   );
