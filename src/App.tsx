4beeb0d9246ecbc2223f488f295020b56ad9c0b9
Antoine Cusset <a.cusset@gmail.com>
2022-03-17 20:44:06 +0800

    Add head and first sound

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import zero from './zero.png';
 import './App.css';
 
 function App() {
+  const audio = new Audio('sub-and-non-sub.wav')
+
+  const handleClick = () => {
+    console.log('clic')
+    audio.play()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header onClick={handleClick} className="App-header">
+        <img src={zero} alt="logo" className='App-logo' />
       </header>
     </div>
   );
